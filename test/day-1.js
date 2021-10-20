cde282a3f6e6db3268139d56b7eff76ac8232b9a
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-20 11:21:01 +0200

    day 1 ex 0

---
+++ test/day-1.js
@@ -0,0 +1,23 @@
+import assert from "assert"
+import { my_sum } from "../day-1/exercice-0.js"
+
+describe('my_sum valid tests', () => {
+ it('should return 2', () => {
+        assert.equal(my_sum(1, 1), 1 + 1);
+    });
+ it('should return 9', () => {
+        assert.equal(my_sum(3.1, 5.9), 3.1 + 5.9);
+    });
+});
+
+describe('my_sum corner tests', () => {
+    it('should return 0', () => {
+        assert.equal(my_sum(69, "1"), 0);
+    });
+    it('should return 0', () => {
+        assert.equal(my_sum(undefined, null), 0);
+    });
+});
+
+/*console.log("my_sum(1, 1) = " + my_sum(1, 1));
+console.log("my_sum(5.9, 4.1) = " + my_sum(5.9, 4.1));*/
