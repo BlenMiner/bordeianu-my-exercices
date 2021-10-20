811fd645c0af490ae5648c34c92214b9f728deea
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-20 12:22:15 +0200

    raw string

--- day-1/exercice-1.js
+++ day-1/exercice-1.js
@@ -1,11 +1,1 @@
-export const my_display_alpha = () => {
-    let result = "";
-    const start = "a".charCodeAt(0);
-    const end = "z".charCodeAt(0);
-
-    for (let c = start; c <= end; ++c) {
-        result += String.fromCharCode(c);
-    }
-
-    return result;
-}
+export const my_display_alpha = () => "abcdefghijklmnopqrstuvwxyz";
