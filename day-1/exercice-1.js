06ed8e680ab1166abe5b39f19a36aa69eafe875e
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-20 11:32:04 +0200

    exo 1

---
+++ day-1/exercice-1.js
@@ -0,0 +1,11 @@
+export const my_display_alpha = () => {
+    let result = "";
+    const start = "a".charCodeAt(0);
+    const end = "z".charCodeAt(0);
+
+    for (let c = start; c <= end; ++c) {
+        result += String.fromCharCode(c);
+    }
+
+    console.log(result);
+}
---
+++ test/day-1-rawtests.js
@@ -0,0 +1,3 @@
+import { my_display_alpha } from "../day-1/exercice-1.js"
+
+console.log(my_display_alpha());
