0dac84b8fcadfd45cee44a2adce80c40d580c317
Valentin BORDEIANU <blenminer123@gmail.com>
2021-10-20 12:00:39 +0200

    exo 4

--- test/day-1-rawtests.js
+++ test/day-1-rawtests.js
@@ -1,7 +1,12 @@
 import { my_display_alpha } from "../day-1/exercice-1.js"
 import { my_display_alpha_reverse } from "../day-1/exercice-2.js"
 import { my_alpha_number } from "../day-1/exercice-3.js"
+import { my_size_alpha } from "../day-1/exercice-4.js"
 
 console.log(my_display_alpha());
 console.log(my_display_alpha_reverse());
 console.log(my_alpha_number(-678.69));
+console.log(my_size_alpha(-678.69));
+console.log(my_size_alpha("Hello"));
+console.log(my_size_alpha("H"));
+console.log(my_size_alpha(""));
